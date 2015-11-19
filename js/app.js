48f45c96c6e033fc8d47e5c8eda977ffdada7a22
Vicente Albiter <vic.albiter@gmail.com>
2015-11-18 21:42:27 -0600

    Add enemy methods to set speed, initial position and movement

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-// Declare global variables to control player's movement
+// Declare useful global variables to control both player and enemy movement
 var distanceX = 101,
     distanceY = 83,
     homeX = 202,
-    homeY = 405;
+    homeY = 405,
+    enemyHomeX = -100,
+    boardWidth = 505,
+    boardHeigh = 606;
+
 
 // Enemies our player must avoid
 var Enemy = function() {
@@ -12,6 +16,16 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // Set the initial x position in which the enemy will appear
+    this.x = enemyHomeX;
+
+    // Set the initial row (y position) in which the enemy will appear
+    this.y = this.setRow();
+
+    // Set enemy initial speed
+    this.speed = this.setSpeed();
+
 };
 
 // Update the enemy's position, required method for game
@@ -20,12 +34,42 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // Move the enemy acording to its speed
+    this.x = this.x + this.speed*dt;
+
+    // Update the row and speed of the enemy when it finishes its run
+    if (this.x > boardWidth) {
+        this.x = enemyHomeX;
+        this.speed = this.setSpeed();
+        this.y = this.setRow();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+// Randomly select row in which the enemy will appear
+Enemy.prototype.setRow = function() {
+
+    // Generate random number between 1 and 3
+    var row = Math.floor((Math.random() * 3) + 1);
+    // Row 1 : 60;   Row 2 : 143;   Row 3 : 226;
+    if (row === 1) { return 60; }
+    else if (row === 2) { return 143; }
+    else if (row === 3) { return 226; }
+};
+
+// Randomly select enemy speed
+Enemy.prototype.setSpeed = function() {
+
+    // Return a multiple of 20 in the range of [40, 200]
+    return (Math.floor((Math.random() * 10) + 2)) * 20;
+
+};
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -71,8 +115,9 @@ Player.prototype.handleInput = function(pressedKey) {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var enemy = new Enemy();
+var allEnemies = [enemy];
 // Place the player object in a variable called player
-var allEnemies = [];
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
